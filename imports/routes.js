6c40f069ca6747ea8e39c8a6273d5de3e2b24fb7
Bruno Silva <bruno5ssilva02@gmail.com>
2024-03-04 11:27:20 -0300

    third commit

--- imports/routes.js
+++ imports/routes.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import PaginaInicial from './ui/PaginaInicial';
+import TelaLogin from './ui/TelaLogin';
 
  export const RoutesApp = () => {
 
     return (
         <>
             <Routes>
-                <Route exact path="/" element={<PaginaInicial />} />
+                <Route exact path="/" element={<TelaLogin />} />
             </Routes>
         </>
     )
---
+++ imports/ui/TelaLogin.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import styles from './TelaLogin.module.css';
+
+export default TelaLogin = () => {
+    return (
+        <div className="main">
+            <div className="cardLogin">
+                <label>Login: </label>
+                <label>Senha: </label>
+            </div>
+        </div>
+    )
+}
