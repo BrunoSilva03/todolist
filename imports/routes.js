e2fba3c814735ee0591fd6462059747b82b50616
Bruno Silva <bruno5ssilva02@gmail.com>
2024-03-14 20:17:51 -0300

    commit today

--- imports/routes.js
+++ imports/routes.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Meteor } from 'meteor/meteor';
 
 import HomePage from './ui/HomePage';
-import RegistrarEmail from './ui/Login/RegistrarEmail';
 import RecuperarSenha from './ui/Login/RecuperarSenha';
 
 
@@ -14,8 +13,6 @@ export const RoutesApp = () => {
         <>
             <Routes>
                 <Route exact path="/" element={<HomePage />} />
-
-                <Route path="/registrar-email" element={<RegistrarEmail />}/>
 
                 <Route path="/recuperar-senha" element={<RecuperarSenha />}/>
             </Routes>
--- imports/ui/BoasVindas.jsx
+++ imports/ui/BoasVindas.jsx
@@ -1,8 +1,8 @@
 import { Meteor } from 'meteor/meteor';
-import React from 'react';
+import React, { useState } from 'react';
 import { Accounts } from 'meteor/accounts-base';
 import { useTracker } from 'meteor/react-meteor-data';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import { toast } from 'react-toastify';
 
 import styles from './BoasVindas.module.css';
@@ -12,25 +12,53 @@ export default BoasVindas = () => {
     const navigate = useNavigate();
 
 
+
+
+
+
     const logout = () => {
         Meteor.logout();
         toast.success('Volte sempre :)');
         navigate('/'); // Redireciona para a página inicial após o logout
+
+    }
+
+    // Accounts.onLogin(() => {
+    //     //voltar para a tela inicial depois que confirmou seu email.
+    //     navigate('/');
+    // })
+
+const abrirPopUpEmail = () => {
+    $(function() { 
         
-    }
+        $('.popupEmail').fadeIn(1000);
+        $('popupEmail').css('display', 'block');
+    });
+}
 
-    Accounts.onLogin(() => {
-        //voltar para a tela inicial depois que confirmou seu email.
-        navigate('/');
-    })
+const fecharPopUpEmail = () => {
+    $(function() {
+        $('.popupEmail').fadeOut(3000);
+        $('.popupEmail').css('display', 'none');
+    });
+}
+   
 
-    const registrarEmail = () => {
-        navigate('/registrar-email');
-    }
+
 
     return (
         <div className="mainBoasVindas">
             <h1>Olá {user.username || user.profile.name} Seja Bem-Vindo ao To Do List</h1>
+
+
+
+
+            <div className="popupEmail">
+                <label>Email: </label>
+                <input type="email" />
+                <button onClick={() => fecharPopUpEmail()}>Enviar</button>
+            </div>
+
 
             <div className="areaCards">
                 <div className="card1">Conteúdo</div>
@@ -39,15 +67,18 @@ export default BoasVindas = () => {
                 <div className="card4"><h3>Visualizar Tarefas</h3></div>
             </div>
 
-           <div className="areaButtonsBoasVindas">
-            {5 > 10 ? (<div className="emailArea">
-                <button className="btnEmailRec" onClick={() => registrarEmail()}><abbr title="Caso esqueça sua senha você poderá entrar novamente utilizando seu email">Adicione seu email de recuperação</abbr></button>
-               </div>) : (
-                <div></div>
-               )}
-               
+
+
+
+            <div className="emailArea">
+
+                <button className="btnEmailRec" onClick={() => abrirPopUpEmail()} ><abbr title="Caso esqueça sua senha você poderá entrar novamente utilizando seu email">Adicione seu email de recuperação</abbr></button>
+
+            </div>
+
+            <div className="areaButtonsBoasVindas">
                 <div className="areaBtnSair"><button onClick={() => logout()} className="btnSair">Sair</button></div>
-           </div>
+            </div>
         </div>
     )
 }
